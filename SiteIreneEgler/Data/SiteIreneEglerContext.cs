e2e36946598b57b6a13b578846b35a2da1cf31d1
Irene Egler <irene.egler@gmail.com>
2023-01-13 15:59:32 -0300

    CRUD Scaffolding

---
+++ SiteIreneEgler/Data/SiteIreneEglerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SiteIreneEgler.Models;
+
+namespace SiteIreneEgler.Data
+{
+    public class SiteIreneEglerContext : DbContext
+    {
+        public SiteIreneEglerContext (DbContextOptions<SiteIreneEglerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SiteIreneEgler.Models.Departament> Departament { get; set; }
+    }
+}
--- SiteIreneEgler/Startup.cs
+++ SiteIreneEgler/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SiteIreneEgler.Data;
 
 namespace SiteIreneEgler
 {
@@ -33,6 +35,9 @@ namespace SiteIreneEgler
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SiteIreneEglerContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SiteIreneEglerContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
