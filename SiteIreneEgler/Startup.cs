db93f34fcce895c1adb4613fe63532808b4af1e4
Irene Egler <irene.egler@gmail.com>
2023-01-13 17:08:22 -0300

    First Migration MySQL

--- SiteIreneEgler/Startup.cs
+++ SiteIreneEgler/Startup.cs
@@ -37,7 +37,8 @@ namespace SiteIreneEgler
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SiteIreneEglerContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SiteIreneEglerContext")));
+                options.UseMySql(Configuration.GetConnectionString("SiteIreneEglerContext"), builder =>
+builder.MigrationsAssembly("SiteIreneEgler")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
