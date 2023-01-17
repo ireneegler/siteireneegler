3af4e1b416e153866a4de1abbb4d3e06bacf815f
Irene Egler <irene.egler@gmail.com>
2023-01-17 11:23:26 -0300

    Seeding Service

--- SiteIreneEgler/Startup.cs
+++ SiteIreneEgler/Startup.cs
@@ -39,14 +39,17 @@ namespace SiteIreneEgler
             services.AddDbContext<SiteIreneEglerContext>(options =>
                 options.UseMySql(Configuration.GetConnectionString("SiteIreneEglerContext"), builder =>
 builder.MigrationsAssembly("SiteIreneEgler")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
