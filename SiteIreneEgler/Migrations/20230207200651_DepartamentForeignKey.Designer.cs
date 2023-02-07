1d094dd59e9773689aa201311e37fc2dc39011e2
Irene Egler <irene.egler@gmail.com>
2023-02-07 17:14:50 -0300

    Foreign key not null

--- SiteIreneEgler/Migrations/20230207200651_DepartamentForeignKey.Designer.cs
+++ SiteIreneEgler/Migrations/20230207200651_DepartamentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SiteIreneEgler.Data;
 
 namespace SiteIreneEgler.Migrations
 {
     [DbContext(typeof(SiteIreneEglerContext))]
-    partial class SiteIreneEglerContextModelSnapshot : ModelSnapshot
+    [Migration("20230207200651_DepartamentForeignKey")]
+    partial class DepartamentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
