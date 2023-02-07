34cd06cbc32332a5f7fafeb8110f0fe13a5b8692
Irene Egler <irene.egler@gmail.com>
2023-02-07 13:52:43 -0300

    Simple Create Form

--- SiteIreneEgler/Services/SellerService.cs
+++ SiteIreneEgler/Services/SellerService.cs
@@ -18,6 +18,12 @@ namespace SiteIreneEgler.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
        
     }
 }
