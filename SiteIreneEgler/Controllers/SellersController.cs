20f1a761e407c0d66950b9d0cf7b0f9dc572e61f
Irene Egler <irene.egler@gmail.com>
2023-02-09 17:23:05 -0300

    Create Edit View

--- SiteIreneEgler/Controllers/SellersController.cs
+++ SiteIreneEgler/Controllers/SellersController.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using SiteIreneEgler.Services;
+using SiteIreneEgler.Services.Exceptions;
 
 namespace SiteIreneEgler.Controllers
 {
@@ -76,6 +77,45 @@ namespace SiteIreneEgler.Controllers
 
             return View(obj);
         }
-           
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Departament> departaments = _departamentService.FinAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departaments = departaments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
--- SiteIreneEgler/Services/SellerService.cs
+++ SiteIreneEgler/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using SiteIreneEgler.Data;
 using SiteIreneEgler.Models;
+using SiteIreneEgler.Services.Exceptions;
 using Microsoft.EntityFrameworkCore;
 
 namespace SiteIreneEgler.Services
@@ -35,6 +36,23 @@ namespace SiteIreneEgler.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e) 
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
 
        
