a467a66713cf348dffa95739cd45ccb215f3b3d9
Irene Egler <irene.egler@gmail.com>
2023-02-10 15:15:03 -0300

    Local, number and date formatting

--- SiteIreneEgler/Models/Seller.cs
+++ SiteIreneEgler/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,7 +11,14 @@ namespace SiteIreneEgler.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+       
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         public Departament Departament { get; set; }
         public int DepartamentId { get; set; }
